bf0c387e2180b9705628acabf0a479e668eb48d2
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2025-08-07 11:11:46 -0300

    Tabela Produto - Criada

--- sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/model/Fornecedor.java
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/model/Fornecedor.java
@@ -1,12 +1,17 @@
 package br.com.dunnastecnologia.sistemapedidosfornecedores.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,10 +40,7 @@ public class Fornecedor {
     @Column(nullable = false, length = 255)
     private String senha;
 
-    /**
-     * Produto ainda não foi implementado, mas aqui está o mapeamento
-     *
-     * @OneToMany( mappedBy = "fornecedor", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY )
-     * private List<Produto> produtos = new ArrayList<>();
-     */
+    @OneToMany(mappedBy = "fornecedor", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Produto> produtos = new ArrayList<>();
+
 }
---
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/model/Produto.java
@@ -0,0 +1,46 @@
+package br.com.dunnastecnologia.sistemapedidosfornecedores.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false, length = 255)
+    private String nome;
+
+    @Column(name = "descricao", columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "preco", nullable = false, precision = 19, scale = 2)
+    private BigDecimal preco;
+
+    @Column(name = "percentual_desconto", nullable = false, precision = 5, scale = 2)
+    private BigDecimal percentualDesconto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fornecedor_id", nullable = false)
+    private Fornecedor fornecedor;
+
+}
