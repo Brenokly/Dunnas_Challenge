645a7f1794e7c114703226ec69b2534a603a8301
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2025-08-07 10:04:41 -0300

    Alterações e Correções na Estrutura.

--- sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/SistemaPedidosFornecedores.java
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/SistemaPedidosFornecedores.java
@@ -1,4 +1,4 @@
-package br.com.dunnastecnologia.desafiofullstackjava;
+package br.com.dunnastecnologia.sistemapedidosfornecedores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaPedidosFornecedores {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaPedidosFornecedores.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaPedidosFornecedores.class, args);
+    }
 
 }
