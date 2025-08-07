add079f4f0be401391c5b986effa6a94fd145966
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2025-08-07 09:43:52 -0300

    Criação da Estrutura do Projeto.

---
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/SistemaPedidosFornecedores.java
@@ -0,0 +1,13 @@
+package br.com.dunnastecnologia.desafiofullstackjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidosFornecedores {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidosFornecedores.class, args);
+	}
+
+}
