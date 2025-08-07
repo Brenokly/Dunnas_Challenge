71e0bb3fb63ebb8b0c50b75e758d8ed654c06494
BrenoKly <breno.moura@alunos.ufersa.edu.br>
2025-08-07 14:02:46 -0300

    Tabela Transacao - Criada

---
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/model/Transacao.java
@@ -0,0 +1,52 @@
+package br.com.dunnastecnologia.sistemapedidosfornecedores.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import br.com.dunnastecnologia.sistemapedidosfornecedores.utils.enums.TipoTransacao;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "transacoes")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "data_transacao", nullable = false)
+    private OffsetDateTime dataTransacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_transacao", nullable = false, length = 100)
+    private TipoTransacao tipoTransacao;
+
+    @Column(name = "valor", nullable = false, precision = 19, scale = 2)
+    private BigDecimal valor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+}
---
+++ sistema-pedidos-fornecedores/src/main/java/br/com/dunnastecnologia/sistemapedidosfornecedores/utils/enums/TipoTransacao.java
@@ -0,0 +1,7 @@
+package br.com.dunnastecnologia.sistemapedidosfornecedores.utils.enums;
+
+public enum TipoTransacao {
+    PAGAMENTO_PEDIDO,
+    ADICAO_SALDO,
+    ESTORNO
+}
